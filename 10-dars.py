787dffbbc8e3a413f407d7327f464ce950127928
Firdavs kenjaev <88057599+Firdavsiyy@users.noreply.github.com>
2021-07-28 01:35:13 +0500

    yangi fayllar
    10-dars yuklandi

---
+++ 10-dars.py
@@ -0,0 +1,55 @@
+# 10-dars if-else
+# avtolar = ['audi','bmw','mercedes','hyundai','toyota']
+# for avto in avtolar:#avtolar ichidan bitta avto uchun
+# if avto == 'bmw':
+# print(avto.upper())
+# else:
+# print(avto.title())
+# if-agar '== tengmi degan manoni anglatadi
+# else: 'aks holda'
+
+# 'a '!='' teng emasmi degani
+# ism = input("ismingiz nima?\n>>>")
+# if ism.lower() == 'firdavs':
+# print(f"uzur,{ism.title()} biz kalini kutyapmiz")
+# else:
+# print('Salom Kali')
+
+# avtolar = ['mercedes', 'audi', 'chevrolet', 'spark']
+# for avto in avtolar:
+# print(avto)
+
+# javob = float(input('12x6 nechiga teng'))
+# if javob != 72:
+#  print('javob xato')
+# else:
+# print('javobingiz togri')
+
+# yosh = int(input('yosingiz necida:'))
+# if yosh <= 18:  # aks holda
+# print('kirish mumkin emas')
+# else:  # yos 18 dan katta yoki teng bolsa
+# print('Xush kelibsiz')
+
+# login = input("yangi login tanlang")
+# if len(login) <= 5:  # 5 tadan kop bolsin
+#       print("login 5 ta harfdan ko'proq bolishi shart")
+# else:
+#          print("xush kelibsiz", login)
+
+# yil = int(input("tug'ilgan yilingizni kiriting:"))
+# ism = input("login kiriting\n>>>")
+# if len(ism.upper())<= 5:
+   #       print("login 5 ta harfdan ko'proq bolishi shart")
+# else:
+   # print('xush kelibsiz',ism.title())
+# if 2021 - yil < 18:
+   # print(f"sizning yoshingiz {2021 - yil},ekan")
+   # print("kirish mumkin emas!")
+
+# yosh = int(input("yoshingiz necida?>>>"))
+# if yosh>65: print("siz covid 19 risk guruhidasiz")
+# else:   print("bosabering:)")
+# x, y = 12,477
+# print("x>y") if x>y else print("x<y")
+
